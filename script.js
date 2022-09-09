77e3f57aa3b757d759b5a5a757d3e140c2609366
BOBURJON <sahodullaevboburzon@gmail.com>
2022-09-09 07:21:35 +0500

    imgs

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ karzinka.classList.add("karzinka");
 nav.append(logo);
 nav.append(link);
 nav.append(karzinka);
+
+
+
+logo.innerHTML += `
+<p>Drile</p>
+
+`
+link.innerHTML += `
+
+<a href="#">Home </a>                
+<a href="#">Shop </a>
+<a href="#">Product</a>
+<a href="#">Blog</a>
+<a href ="#">Page</a>
+<a href ="#">Contact</a>
+
+`
+karzinka.innerHTML +=`
+<button><img src="./imgs/Vector (1).png" alt=""></button>
+<button><img src="./imgs/Group.png" alt=""></button>
+<button><img src="./imgs/Vector.png" alt=""></button>
+
+
+`
