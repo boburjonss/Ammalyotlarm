425c9160c779a07ba502610b3c380b1b29e76600
BOBURJON <sahodullaevboburzon@gmail.com>
2022-09-10 19:57:19 +0500

    img22

--- script.js
+++ script.js
@@ -10,16 +10,12 @@ logo.classList.add("logo");
 link.classList.add("link");
 karzinka.classList.add("karzinka");
 
-
-
 nav.append(logo);
 nav.append(link);
 nav.append(karzinka);
 
-
-
 logo.innerHTML += `
-<p>Drile</p>
+<img src="./imgs/Drile.png" alt="">
 
 `
 link.innerHTML += `
@@ -37,5 +33,44 @@ karzinka.innerHTML +=`
 <button><img src="./imgs/Group.png" alt=""></button>
 <button><img src="./imgs/Vector.png" alt=""></button>
 
+
+`
+
+const section1 = document.querySelector('.section1');
+const left = document.createElement('div');
+const center = document.createElement('div');
+const right = document.createElement('div');
+const divimg = document.createElement('div')
+const divtext = document.createElement('div')
+
+section1.classList.add('section1');
+left.classList.add('left');
+center.classList.add('center');
+right.classList.add('right');
+divimg.classList.add('divimg');
+divtext.classList.add('divtext');
+
+
+section1.append(left);
+section1.append(center);
+section1.append(right);
+center.append(divimg);
+center.append(divtext);
+
+left.innerHTML +=`
+<img src="./imgs/Vector (2).png" alt="">
+`
+right.innerHTML +=`
+<img src="./imgs/Vector (3).png" alt="">
+
+`
+divimg.innerHTML +=`
+<img src="./imgs/img1.png" alt="">
+
+`
+divtext.innerHTML +=`
+<p>Get ready for Our
+stylist chair</p>
+<button>SHOP NOW</button>
 
 `
